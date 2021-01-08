65b3a16cc89900104fbb93eeb1d5657541a74dfb
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-08 17:48:18 -0500

    Update PrepareCraftEvent.java

--- src/me/TurtlesAreHot/BrickThrower/events/PrepareCraftEvent.java
+++ src/me/TurtlesAreHot/BrickThrower/events/PrepareCraftEvent.java
@@ -1,7 +1,5 @@
 package me.TurtlesAreHot.BrickThrower.events;
 
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
@@ -11,34 +9,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+
+import me.TurtlesAreHot.BrickThrower.GeneralMethods;
 
 public class PrepareCraftEvent implements Listener {
 	@EventHandler
 	public void onCraft(PrepareItemCraftEvent e) {
+		String server_ver = Bukkit.getVersion();
+		String version = server_ver.substring(server_ver.indexOf("(MC: ")+5, server_ver.indexOf("(MC: ")+9); // gets the version without the end number.
+		//For some unknown reason to me, on 1.16 the lore gets set to "on_ft" instead... I really cannot figure it out. Any ideas please reply!
 		for (HumanEntity entity : e.getViewers()) {
 			if (entity instanceof Player) {
 				CraftingInventory g = e.getInventory();
 				for (ItemStack item : g.getContents()) {
-					ItemMeta meta = item.getItemMeta();
-					if(item.getType() == Material.BRICK && meta.getDisplayName().equals("Heavy Brick")) {
-						List<String> lores = meta.getLore();
-						for (String lore : lores) {
-							String fix_lore = lore.replaceAll("§", "");
-							String server_ver = Bukkit.getVersion();
-							String version = server_ver.substring(server_ver.indexOf("(MC: ")+5, server_ver.indexOf("(MC: ")+9); // gets the version without the end number.
-							//For some unknown reason to me, on 1.16 the lore gets set to "on_ft" instead... I really cannot figure it out. Any ideas please reply!
-							if(version.equals("1.16")) {
-								if(fix_lore.equals("on_ft")) {
-									e.getInventory().setResult(new ItemStack(Material.AIR));
-									break;
-								}
-							}
-							else if (fix_lore.equals("no_craft")) {
-								e.getInventory().setResult(new ItemStack(Material.AIR));
-								break;
-							}
-						}
+					String info = null;
+					if (version.equalsIgnoreCase("1.13")) {
+						info = GeneralMethods.getNBTDataString13(item, "brickthrower_item");
+					}
+					else {
+						info = GeneralMethods.getNBTDataString(item, "brickthrower_item");
+					}
+					if(info != null) {
+						e.getInventory().setResult(new ItemStack(Material.AIR));
+						break;
 					}
 				}
 			}
