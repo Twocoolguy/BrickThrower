555309991790b14dcbd0307bcadb50eb475288f9
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-14 15:00:11 -0500

    Config Class
    This is simply where you get everything in the config.

--- src/me/TurtlesAreHot/BrickThrower/Config.java
+++ src/me/TurtlesAreHot/BrickThrower/Config.java
@@ -7,38 +7,30 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Config {
-	static Material default_item;
-	static List<String> material_list;
-	static int bricks_given;
-	static boolean reload_enabled;
-	static String item_name;
+	
+	private static FileConfiguration config;
 	
 	public static void reloadConfig() {
-		FileConfiguration config = JavaPlugin.getPlugin(Main.class).getConfig();
-		default_item = Material.getMaterial(config.getString("default-item"));
-		material_list = config.getStringList("items");
-		bricks_given = config.getInt("bricks-given");
-		reload_enabled = config.getBoolean("reload-enabled");
-		item_name = config.getString("item-name");
+		config = JavaPlugin.getPlugin(Main.class).getConfig();
 	}
 	
 	public static Material getDefaultItem() {
-		return default_item;
+		return Material.getMaterial(config.getString("default-item"));
 	}
 	
 	public static List<String> getMaterialList() {
-		return material_list;
+		return config.getStringList("items");
 	}
 	
 	public static int getBricksGiven() {
-		return bricks_given;
+		return config.getInt("bricks-given");
 	}
 	
 	public static boolean isReloadEnabled() {
-		return reload_enabled;
+		return config.getBoolean("reload-enabled");
 	}
 	
 	public static String getItemName() {
-		return item_name;
+		return config.getString("item-name");
 	}
 }
