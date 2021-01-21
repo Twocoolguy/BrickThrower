2007f6d2f2640acf817f48e31bb80269c0fe8525
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-21 15:17:44 -0500

    Refactored

--- src/me/TurtlesAreHot/BrickThrower/events/PlayerClickEvent.java
+++ src/me/TurtlesAreHot/BrickThrower/events/PlayerClickEvent.java
@@ -1,7 +1,6 @@
 package me.TurtlesAreHot.BrickThrower.events;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,6 +10,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.TurtlesAreHot.BrickThrower.Config;
 import me.TurtlesAreHot.BrickThrower.GeneralMethods;
 import me.TurtlesAreHot.BrickThrower.Main;
 
@@ -18,16 +18,15 @@ public class PlayerClickEvent implements Listener {
 	
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent event) {
-		Player p = event.getPlayer();
 		if (event.getItem() == null) {
 			// Checks if the event item is air, if it is we do not want to do anything because the program will error.
 			return;
 		}
+		Player p = event.getPlayer();
 		// Getting item in mainhand, this gets exactly what it is with the names.
 		ItemStack held = p.getInventory().getItemInMainHand();
 		String info = null;
-		String server_ver = Bukkit.getVersion();
-		String version = server_ver.substring(server_ver.indexOf("(MC: ")+5, server_ver.indexOf("(MC: ")+9);
+		String version = Config.getServerVersion(); // Gives us the server version.
 		if (version.equalsIgnoreCase("1.13")) {
 			info = GeneralMethods.getNBTDataString13(held, "brickthrower_item");
 		}
@@ -38,22 +37,26 @@ public class PlayerClickEvent implements Listener {
 			// Creates a itemstack of bricks with just one ands sets up all information about the brick.
 			ItemStack brick = new ItemStack(held.getType(), 1);
 			ItemMeta brick_im = brick.getItemMeta();
-			brick_im.setDisplayName("Heavy Brick");
+			brick_im.setDisplayName(Config.getItemName());
 			brick.setItemMeta(brick_im);
 			// Gets the location of the brick and then sets its velocity and pickupdelay to make the item launch, and make it so you cannot pick it up.
-			Location brick_loc = p.getEyeLocation();
-			Item brock = p.getWorld().dropItem(brick_loc, brick);
-			brock.setVelocity(brick_loc.getDirection());
-			brock.setPickupDelay(Short.MAX_VALUE);
+			Item brock = p.getWorld().dropItem(p.getEyeLocation(), brick);
+			brock.setVelocity(p.getEyeLocation().getDirection());
+			int itemTime = Config.getDisappearTime();
+			if(itemTime != 0) {
+				brock.setPickupDelay(Short.MAX_VALUE);
+			}
 			// Removes one of the bricks from the players inventory.
 			event.getItem().setAmount(event.getItem().getAmount()-1);
-			// After 2 seconds remove the brick.
-			Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class),  new Runnable() {
-				@Override
-				public void run() {
-					brock.remove();
-				}
-			}, 2*20L);
+			// After the time in seconds defined in the config remove the brick.
+			if(itemTime != 0) {
+				Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class),  new Runnable() {
+					@Override
+					public void run() {
+						brock.remove();
+					}
+				}, itemTime*20L);
+			}
 		}
 		
 	}
