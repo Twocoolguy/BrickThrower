089ef6d0acd58b7f4110fdbe2cd07d39918729e7
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-30 15:20:29 -0500

    Bug Patches for more version support

--- src/me/TurtlesAreHot/BrickThrower/events/PlayerClickEvent.java
+++ src/me/TurtlesAreHot/BrickThrower/events/PlayerClickEvent.java
@@ -23,30 +23,57 @@ public class PlayerClickEvent implements Listener {
 			return;
 		}
 		Player p = event.getPlayer();
-		// Getting item in mainhand, this gets exactly what it is with the names.
-		ItemStack held = p.getInventory().getItemInMainHand();
-		String info = null;
 		String version = Config.getServerVersion(); // Gives us the server version.
+		if(version.equals("1.8")) {
+			if(p.getInventory().getItem(p.getInventory().getHeldItemSlot()) == null) {
+				return;
+			}
+		}
+		else if (p.getInventory().getItemInMainHand() == null) {
+			return;
+		}
+		// Getting item in mainhand, this gets exactly what it is with the names.
+		ItemStack held = null;
+		if(version.equals("1.8")) {
+			held = p.getInventory().getItem(p.getInventory().getHeldItemSlot());
+		}
+		else {
+			held = p.getInventory().getItemInMainHand();
+		}
+		String info = null;
 		switch(version) {
 			case "1.13":
 				info = NBT13.getNBTDataString(held, "brickthrower_item");
+				break;
 			case "1.12":
 				info = NBT12.getNBTDataString(held, "brickthrower_item");
+				break;
 			case "1.11":
 				info = NBT11.getNBTDataString(held, "brickthrower_item");
+				break;
 			case "1.10":
 				info = NBT10.getNBTDataString(held, "brickthrower_item");
+				break;
 			case "1.9":
 				info = NBT9.getNBTDataString(held, "brickthrower_item");
+				break;
 			case "1.8":
 				info = NBT8.getNBTDataString(held, "brickthrower_item");
+				break;
 			default:
 				info = NBT14.getNBTDataString(held, "brickthrower_item");
+
 		}
-		if(info != null) {
+		if(info == null) {
+			return;
+		}
+		if(info.equals("true")) {
 			// Creates a itemstack of bricks with just one ands sets up all information about the brick.
 			ItemStack brick = new ItemStack(held.getType(), 1);
 			ItemMeta brick_im = brick.getItemMeta();
+			if (brick_im == null) {
+				return;
+			}
 			brick_im.setDisplayName(Config.getItemName());
 			brick.setItemMeta(brick_im);
 			// Gets the location of the brick and then sets its velocity and pickupdelay to make the item launch, and make it so you cannot pick it up.
@@ -57,7 +84,16 @@ public class PlayerClickEvent implements Listener {
 				brock.setPickupDelay(Short.MAX_VALUE);
 			}
 			// Removes one of the bricks from the players inventory.
-			event.getItem().setAmount(event.getItem().getAmount()-1);
+			ItemStack eventItem = event.getItem();
+			if(eventItem.equals(held)) {
+				// This checks if the item that is trying to be thrown is in the off hand. Otherwise it doesn't throw it and it also does not remove any.
+				if(!(Config.twelveAndAbove()) && held.getAmount() == 1) {
+					event.getPlayer().getInventory().remove(held);
+				}
+				else {
+					event.getItem().setAmount(held.getAmount()-1);
+				}
+			}
 			// After the time in seconds defined in the config remove the brick.
 			if(itemTime != 0) {
 				Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class),  new Runnable() {
