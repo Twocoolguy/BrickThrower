50f016d64f47312311b99487f677024649ddc4f3
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-30 17:21:07 -0500

    Support for older ids

--- src/me/TurtlesAreHot/BrickThrower/Main.java
+++ src/me/TurtlesAreHot/BrickThrower/Main.java
@@ -14,27 +14,33 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		Config.reloadVersion();
 		this.saveDefaultConfig(); // Creates config.yml
 		FileConfiguration config = this.getConfig();
 		config.addDefault("bricks-given", 10);
 		config.addDefault("reload-enabled", true);
 		config.addDefault("item-name", "Heavy Brick");
 		List<String> materials = new ArrayList<String>();
-		materials.add("BRICK");
-		materials.add("NETHER_BRICK");
+		String default_item_mat = "BRICK";
+		if(Config.oldServer()) {
+			materials.add("CLAY_BRICK");
+			materials.add("NETHER_BRICK_ITEM");
+			default_item_mat = "CLAY_BRICK";
+		}
+		else {
+			materials.add("BRICK");
+			materials.add("NETHER_BRICK");
+		}
 		config.addDefault("items", materials); // This list contains all of the materials that you can get from /brickthrower get.
-		config.addDefault("default-item", "BRICK"); // this is the default item that /brickthrower get will give.
+		config.addDefault("default-item", default_item_mat); // this is the default item that /brickthrower get will give.
 		config.addDefault("item-disappear-time", 2); // time until the item on the ground disappears. Put 0 to disable and allow pickup of the item.
-		
+
 		config.options().copyDefaults(true);
 		this.saveConfig();
 		this.getServer().getPluginManager().registerEvents(new PlayerClickEvent(),  this); // Adding event listener for any listener in the Main class.
 		this.getServer().getPluginManager().registerEvents(new PrepareCraftEvent(), this);
 		getCommand("brickthrower").setExecutor(new BrickThrowerCommand());
 		Config.reloadConfig(); // Sets up our configreader object in our Config class.
-		if(Config.oldServer()) {
-			this.getServer().getLogger().warning("[BrickThrower] This server is on 1.12 or below. You will only be able to throw the brick material because of ID changes in those versions. We do not yet have support for this.");
-		}
 	}
 	
 	@Override
