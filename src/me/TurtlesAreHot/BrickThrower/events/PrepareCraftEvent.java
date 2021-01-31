dca006317f20ab37b7e89d4600cf9c1f0a7209c5
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-01-30 21:57:36 -0500

    Update PrepareCraftEvent.java

--- src/me/TurtlesAreHot/BrickThrower/events/PrepareCraftEvent.java
+++ src/me/TurtlesAreHot/BrickThrower/events/PrepareCraftEvent.java
@@ -1,6 +1,5 @@
 package me.TurtlesAreHot.BrickThrower.events;
 
-import me.TurtlesAreHot.BrickThrower.version.*;
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
@@ -19,30 +18,7 @@ public class PrepareCraftEvent implements Listener {
 			if (entity instanceof Player) {
 				CraftingInventory g = e.getInventory();
 				for (ItemStack item : g.getContents()) {
-					String info = null;
-					String version = Config.getServerVersion();
-					switch(version) {
-						case "1.13":
-							info = NBT13.getNBTDataString(item, "brickthrower_item");
-							break;
-						case "1.12":
-							info = NBT12.getNBTDataString(item, "brickthrower_item");
-							break;
-						case "1.11":
-							info = NBT11.getNBTDataString(item, "brickthrower_item");
-							break;
-						case "1.10":
-							info = NBT10.getNBTDataString(item, "brickthrower_item");
-							break;
-						case "1.9":
-							info = NBT9.getNBTDataString(item, "brickthrower_item");
-							break;
-						case "1.8":
-							info = NBT8.getNBTDataString(item, "brickthrower_item");
-							break;
-						default:
-							info = NBT14.getNBTDataString(item, "brickthrower_item");
-					}
+					String info = Config.getNBTData(item, "brickthrower_item");
 					if(info != null) {
 						e.getInventory().setResult(new ItemStack(Material.AIR));
 						break;
