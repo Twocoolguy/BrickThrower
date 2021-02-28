afde1b1b2a8bd35d4beee9a602a2658eb53769d0
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-02-28 15:37:10 -0500

    Create AnvilEvent.java

---
+++ src/me/TurtlesAreHot/BrickThrower/events/AnvilEvent.java
@@ -0,0 +1,24 @@
+package me.TurtlesAreHot.BrickThrower.events;
+
+import me.TurtlesAreHot.BrickThrower.Config;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.ItemStack;
+
+public class AnvilEvent implements Listener {
+    @EventHandler
+    public void onAnvil(PrepareAnvilEvent e) {
+        ItemStack[] contents = e.getInventory().getContents();
+        for(int i = 0; i < contents.length; i++) {
+            if(contents[i] != null) {
+                if(Config.getNBTData(contents[i], "brickthrower_item") != null) {
+                    e.setResult(new ItemStack(Material.AIR));
+                }
+            }
+        }
+
+    }
+}
