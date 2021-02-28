3f578c3a54d3ea5cfc54b2db5fea5012db15c235
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-02-28 16:09:06 -0500

    BrewingEvent and BrewingFuelEvent

--- src/me/TurtlesAreHot/BrickThrower/Main.java
+++ src/me/TurtlesAreHot/BrickThrower/Main.java
@@ -42,6 +42,8 @@ public class Main extends JavaPlugin {
 		this.getServer().getPluginManager().registerEvents(new EnchantEvent(), this);
 		this.getServer().getPluginManager().registerEvents(new FurnaceSmelt(), this);
 		this.getServer().getPluginManager().registerEvents(new AnvilEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new BrewingFuelEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new BrewingEvent(), this);
 		if(Config.sixteenAndAbove()) {
 			this.getServer().getPluginManager().registerEvents(new SmithingEvent(), this);
 		}
