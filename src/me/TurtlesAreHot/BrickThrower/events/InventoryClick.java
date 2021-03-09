93b660d8f1cbebf2c45cfced13c6ff850fd1d1a7
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-03-09 13:49:35 -0500

    Removed ability to use armor

--- src/me/TurtlesAreHot/BrickThrower/events/InventoryClick.java
+++ src/me/TurtlesAreHot/BrickThrower/events/InventoryClick.java
@@ -4,8 +4,10 @@ import me.TurtlesAreHot.BrickThrower.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryClick implements Listener {
     @EventHandler
@@ -22,6 +24,33 @@ public class InventoryClick implements Listener {
             cit == InventoryType.LOOM) {
             if(Config.getNBTData(e.getCurrentItem(), "brickthrower_item") != null) {
                 e.setCancelled(true);
+            }
+        }
+        //slots for armor:
+        //helmet: 39
+        //chestplate: 38
+        //leggings: 37
+        //boots: 36
+        //Client side it may look like the armor still goes on sometimes, but really it isn't there.
+        if(e.getAction() == InventoryAction.PLACE_ALL ||
+                e.getAction() == InventoryAction.PLACE_ONE ||
+                e.getAction() == InventoryAction.PLACE_SOME ||
+        e.getAction() == InventoryAction.SWAP_WITH_CURSOR) {
+            if (e.getClickedInventory().getType() != null) {
+                InventoryType it = e.getClickedInventory().getType();
+                if (it == InventoryType.PLAYER) {
+                    if (e.getCursor() != null) {
+                        if(Config.getNBTData(e.getCursor(), "brickthrower_item") != null) {
+                            int slotClicked = e.getSlot();
+                            if(slotClicked == 39 ||
+                            slotClicked == 38 ||
+                            slotClicked == 37 ||
+                            slotClicked == 36) {
+                                e.setCancelled(true);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
