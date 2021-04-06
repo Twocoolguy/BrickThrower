d8b89dfc9455b0488ab6dd1d7c8ec44b5eb4720f
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-04-05 22:19:01 -0400

    Update AnvilEvent.java

--- src/me/TurtlesAreHot/BrickThrower/events/AnvilEvent.java
+++ src/me/TurtlesAreHot/BrickThrower/events/AnvilEvent.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 
 public class AnvilEvent implements Listener {
