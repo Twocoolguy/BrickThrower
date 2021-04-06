23cdcfdec03b38f418aa712cf5b239cbc6662379
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-04-05 22:20:51 -0400

    Update InventoryClick.java

--- src/me/TurtlesAreHot/BrickThrower/events/InventoryClick.java
+++ src/me/TurtlesAreHot/BrickThrower/events/InventoryClick.java
@@ -1,28 +1,25 @@
 package me.TurtlesAreHot.BrickThrower.events;
 
 import me.TurtlesAreHot.BrickThrower.Config;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.ItemStack;
 
 public class InventoryClick implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent e) {
-        if(e.getInventory() == null) {
+        if (e.getInventory() == null) {
             return;
         }
-        if(e.getInventory().getType() == null) {
+        if (e.getInventory().getType() == null) {
             return;
         }
         InventoryType cit = e.getInventory().getType();
-        if(cit == InventoryType.STONECUTTER ||
-            cit == InventoryType.CARTOGRAPHY ||
-            cit == InventoryType.LOOM) {
-            if(Config.getNBTData(e.getCurrentItem(), "brickthrower_item") != null) {
+        if (cit == InventoryType.STONECUTTER ||
+                cit == InventoryType.CARTOGRAPHY ||
+                cit == InventoryType.LOOM) {
+            if (Config.getNBTData(e.getCurrentItem(), "brickthrower_item") != null) {
                 e.setCancelled(true);
             }
         }
